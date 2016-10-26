b6e450326fb3790cd3ec28adb82b803abd0d2276
Ryu_S3ki <rbe.jz.gn@gmail.com>
2016-10-26 12:28:17 +0200

    Update menu_operations.c

--- menu_operations.c
+++ menu_operations.c
@@ -159,8 +159,8 @@ void choose_net(struct ap_scan_info array_wifi[], int error){
       menu();
       int bytes = data_read(kb);
 			printf("Char3: %s\n", kb);
-			int option = (int) kb;
-			printf("%d\n", kb);
+			//int option = (int) kb;
+			printf("%s\n", kb);
 			
 			if(bytes < 1){
 				printf("Error de lectura\n");
